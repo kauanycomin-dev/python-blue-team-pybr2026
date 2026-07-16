768bff8abdcc8f773d455e13846f02fdaf740641
Kauany Comin <kauany.comin@al.infnet.edu.br>
2026-07-16 18:23:57 +0000

    refatora reader.py usando with open para fechar arquivo corretamente

--- src/reader.py
+++ src/reader.py
@@ -5,21 +5,22 @@ class LogReader:
 
     def __init__(self, caminho_arquivo, id_cliente):
         self.id_cliente = id_cliente 
-        self.arquivo = open(caminho_arquivo, "r")
-        self.arquivo.seek(0, 2)
+        self.caminho_arquivo = caminho_arquivo
 
     def monitorar(self):
-        while True:
-            linha = self.arquivo.readline()
-            if linha:
-                linha_limpa = linha.strip()
-                if linha_limpa:
-                    yield {
-                        "id_cliente": self.id_cliente,
-                        "log_raw": linha_limpa,
-                        "timestamp": datetime.now().isoformat()
-                    }
-            else:
-                time.sleep(0.1)
+        with open(self.caminho_arquivo, "r") as arquivo:
+            arquivo.seek(0, 2)
+            while True:
+                linha = arquivo.readline()
+                if linha:
+                    linha_limpa = linha.strip()
+                    if linha_limpa:
+                        yield {
+                            "id_cliente": self.id_cliente,
+                            "log_raw": linha_limpa,
+                            "timestamp": datetime.now().isoformat()
+                        }
+                else:
+                    time.sleep(0.1)
 
                  
