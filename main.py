1f4525dccf34936e469c0f086828d58def2e7b7d
Kauany Comin <kauany.comin@al.infnet.edu.br>
2026-07-17 14:27:24 +0000

    atualiza main.py com tag_empresa e origem no INSERT

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from src.database import Database
 db = Database("cyber_security.db")
 db.inicializar_tabela()
 
-log = LogReader("logs/mock_nginx.log", "id_cliente")
+log = LogReader("logs/mock_nginx.log", "id_cliente", "tag_empresa", "origem")
 for pacote in log.monitorar():
     db.cursor.execute(
-        "INSERT INTO logs (id_cliente, log_raw, timestamp) VALUES (?, ?, ?)",
-        (pacote["id_cliente"], pacote["log_raw"], pacote["timestamp"])
+        "INSERT INTO logs (id_cliente, tag_empresa, origem, log_raw, timestamp) VALUES (?, ?, ?, ?, ?)",
+        (pacote["id_cliente"], pacote["tag_empresa"], pacote["origem"], pacote["log_raw"], pacote["timestamp"])
     )
     db.conexao.commit()
